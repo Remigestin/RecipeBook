5e230b9804b3214da9598e7c338e616bde5682b1
Gestin <gestin.remi@gmail.com>
2018-12-16 10:38:32 +0100

    Ajout du fichier connexion à la BD

---
+++ src/Connexion/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package Connexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Deny Prasetyo
+ */
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3306/testdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private String username = "root";
+    private String password = "admin";
+
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driverr");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
+}
