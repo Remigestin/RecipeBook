9727e11547085155820310b411ccd927618ea897
Gestin <gestin.remi@gmail.com>
2018-12-16 11:10:56 +0100

    Ajout d'un fichier test de la bd

--- src/Connection/DatabaseConnection.java
+++ src/Connection/DatabaseConnection.java
@@ -9,13 +9,13 @@ public class DatabaseConnection {
 
     private static DatabaseConnection instance;
     private Connection connection;
-    private String url = "jdbc:mysql://localhost:3306/testdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private String url = "jdbc:mysql://localhost:3306/recipebook?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private String username = "root";
     private String password = "admin";
 
     private DatabaseConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.jdbc.Driverr");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             this.connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException ex) {
             System.out.println("Database Connection Creation Failed : " + ex.getMessage());
