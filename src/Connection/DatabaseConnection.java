3ddf9efb61a93cfddb6863b4a952104230385571
Gestin <gestin.remi@gmail.com>
2018-12-16 10:41:15 +0100

    Renommage connexion -> connection

--- src/Connection/DatabaseConnection.java
+++ src/Connection/DatabaseConnection.java
@@ -1,13 +1,10 @@
-package Connexion;
+package Connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Deny Prasetyo
- */
+
 public class DatabaseConnection {
 
     private static DatabaseConnection instance;
