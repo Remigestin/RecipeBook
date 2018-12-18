9cc6c533b95bf1f988725dfc3d1c9718828cf16b
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-12-18 10:54:52 +0100

    STYLE

--- src/controller/HomePageController.java
+++ src/controller/HomePageController.java
@@ -18,6 +18,5 @@ public class HomePageController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
 	}
 }
--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -8,22 +8,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 
 import abstractDAO.AbstractUserDAO;
 import businessLogic.User;
 import factory.AbstractFactory;
 import factory.MySQLFactory;
-import javafx.event.ActionEvent;
-
+import javafx.event.Event;
 import javafx.scene.control.PasswordField;
 
 public class LoginPageController {
@@ -35,9 +31,18 @@ public class LoginPageController {
 	@FXML
 	private Button login;
 
+	// Event Listener on TextField[#password].HeyPressed
+	@FXML
+	void loginWithEnterKey(KeyEvent event) {
+
+		if (event.getCode() == KeyCode.ENTER) {
+			this.login(event);
+		}
+	}
+
 	// Event Listener on Button[#login].onAction
 	@FXML
-	public void login(ActionEvent event) {
+	public void login(Event event) {
 
 		AbstractFactory f = new MySQLFactory();
 
@@ -47,30 +52,36 @@ public class LoginPageController {
 
 		if (user == null) {
 
-			username.clear();
 			password.clear();
 			this.displayError();
 
 		} else {
-			Parent root;
-			try {
-				
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomePage.fxml"));
-				
-				root = loader.load();
-				HomePageController controller = loader.getController();
-				controller.setUsername(username.getText());
-				
-				Scene scene = new Scene(root, 1920, 1080);
+			this.switchToHomePage(event);
+		}
+	}
 
-				Stage newStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	public void switchToHomePage(Event event) {
 
-				newStage.setScene(scene);
-				newStage.show();
+		Parent root;
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomePage.fxml"));
+
+			root = loader.load();
+
+			HomePageController controller = loader.getController();
+			controller.setUsername(username.getText());
+
+			Scene scene = new Scene(root, 1920, 1080);
+
+			Stage newStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			newStage.setScene(scene);
+			newStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
