4711a9944e8b20ec42597cdd610a2024a8a38546
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-12-22 15:00:28 +0100

    ADD singleton MySQLUserDAO

--- src/abstractDAO/AbstractUserDAO.java
+++ src/abstractDAO/AbstractUserDAO.java
@@ -1,13 +1,10 @@
 package abstractDAO;
 
-import java.sql.SQLException;
-
 import businessLogic.User;
 import exception.DAOException;
 
 public abstract class AbstractUserDAO {
-	
-	public abstract User login(String username, String password) throws DAOException;
 
+	public abstract User login(String username, String password) throws DAOException;
 
 }
--- src/mySQLDAO/MySQLUserDAO.java
+++ src/mySQLDAO/MySQLUserDAO.java
@@ -5,20 +5,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 import abstractDAO.AbstractUserDAO;
 import businessLogic.User;
 import connection.DatabaseConnection;
 import exception.DAOException;
 
 public class MySQLUserDAO extends AbstractUserDAO {
-	
-	//sql
 
+	private static MySQLUserDAO userDAO = null;
+
+	// private constructor
+	private MySQLUserDAO() {
+	}
+
+	// sql
 	private static final String SQL_FIND_BY_USERNAME_AND_PASSWORD = "Select * FROM User WHERE username = ? AND password = ?";
 
-	//actions
-	
+	// actions
 	@Override
 	public User login(String username, String password) throws DAOException {
 
@@ -34,8 +37,7 @@ public class MySQLUserDAO extends AbstractUserDAO {
 			if (rs.next()) {
 				user = map(rs);
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DAOException(e);
 		}
 		return user;
@@ -46,14 +48,12 @@ public class MySQLUserDAO extends AbstractUserDAO {
 		user.setId(resultSet.getInt("id"));
 		user.setUsername(resultSet.getString("username"));
 		user.setFirstname(resultSet.getString("firstname"));
-        user.setLastname(resultSet.getString("lastname"));
-        if (resultSet.getInt("isAdmin") == 1) {
-        	user.setAdmin(true);
-        }
+		user.setLastname(resultSet.getString("lastname"));
+		if (resultSet.getInt("isAdmin") == 1) {
+			user.setAdmin(true);
+		}
 		return user;
 	}
-
-
 
 	public static void main(String args[]) {
 		AbstractUserDAO dao = new MySQLUserDAO();
@@ -61,8 +61,14 @@ public class MySQLUserDAO extends AbstractUserDAO {
 		User marine = dao.login("marine", "123");
 		System.out.println(marine.getUsername());
 
-
-
 	}
 
+	public static MySQLUserDAO getInstance() {
+
+		if (userDAO == null) {
+			userDAO = new MySQLUserDAO();
+		}
+
+		return userDAO;
+	}
 }
