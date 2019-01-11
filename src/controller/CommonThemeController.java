0fc55d442060f590fb0260c25b3fb78f4ddb82da
Ines Missoum <inesmiss@live.fr>
2019-01-11 09:31:04 +0100

    UPDATE view Favorites page

--- src/controller/CommonThemeController.java
+++ src/controller/CommonThemeController.java
@@ -62,7 +62,7 @@ public class CommonThemeController implements ControllerInterface {
 				break;
 	
 			case "randomMenu":
-				newPage = "/views/RandomMenu.fxml";
+				// newPage = "/views/RandomMenu.fxml";
 				break;
 	
 			case "shoppingList":
@@ -89,7 +89,7 @@ public class CommonThemeController implements ControllerInterface {
 			root = loader.load();
 
 			ControllerInterface newController = loader.getController();
-			//newController.setUsername(username.getText());
+			newController.setUsername(username.getText());
 
 			Scene scene = new Scene(root, 1920, 1080);
 
