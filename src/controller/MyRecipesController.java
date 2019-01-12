2e93785d959f0888b385213e632b5cca615f2cc7
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2019-01-12 14:47:29 +0100

    ADD filter recipes with course category on MyRecipes view

--- src/controller/MyRecipesController.java
+++ src/controller/MyRecipesController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import businessLogic.Recipe;
@@ -16,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -42,7 +45,18 @@ public class MyRecipesController implements Initializable {
 	@FXML
 	private TableColumn<Recipe, Integer> difficulty;
 
+	@FXML
+	private ComboBox<String> filter;
+
 	private RecipeFacade facade = RecipeFacade.getInstance();
+
+	public void setFilter() {
+
+		HashMap<Integer, String> courses = facade.findAllCourseCategory();
+		ObservableList<String> choices = FXCollections.observableArrayList(courses.values());
+		choices.add(0, "all recipes");
+		filter.setItems(choices);
+	}
 
 	@FXML
 	void addRecipe(Event event) {
@@ -67,13 +81,25 @@ public class MyRecipesController implements Initializable {
 
 	}
 
-	public ObservableList<Recipe> getRecipes() {
+	public ObservableList<Recipe> getAllRecipes() {
 
 		ObservableList<Recipe> recipes = FXCollections.observableArrayList();
-
 		recipes.addAll(User.getSession().getCreateList());
-
 		return recipes;
+
+	}
+
+	public ObservableList<Recipe> getRecipesByFilterCourseSelected(int idCourseSelected) {
+
+		ObservableList<Recipe> recipesFiltered = FXCollections.observableArrayList();
+
+		for (Recipe r : User.getSession().getCreateList()) {
+
+			if (r.getIdCourse() == idCourseSelected)
+				recipesFiltered.add(r);
+		}
+
+		return recipesFiltered;
 
 	}
 
@@ -111,6 +137,35 @@ public class MyRecipesController implements Initializable {
 		return User.getSession().getCreateList().get(rowNumber).getIdRecipe();
 	}
 
+	@FXML
+	void filterRecipesByCourseCategory(Event event) {
+
+		if (filter.getValue().equals("all recipes")) {
+			listRecipes.setItems(getAllRecipes());
+			
+		} else {
+			int idCourseSelected = getIdCourseByCourseNameSelected();
+			listRecipes.setItems(getRecipesByFilterCourseSelected(idCourseSelected));
+		}
+		
+		listRecipes.refresh();
+	}
+
+	private int getIdCourseByCourseNameSelected() {
+
+		HashMap<Integer, String> allCourses = facade.findAllCourseCategory();
+
+		int idCourseSelected = 0;
+
+		for (Object id : allCourses.keySet()) {
+			if (allCourses.get(id).equals(filter.getValue())) {
+				idCourseSelected = (int) id;
+			}
+		}
+
+		return idCourseSelected;
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
@@ -118,7 +173,8 @@ public class MyRecipesController implements Initializable {
 		// rating.setCellValueFactory(new PropertyValueFactory<Recipe, String>(""));
 		preparationTime.setCellValueFactory(new PropertyValueFactory("preparationTime"));
 		difficulty.setCellValueFactory(new PropertyValueFactory("difficulty"));
-		listRecipes.setItems(getRecipes());
+		listRecipes.setItems(getAllRecipes());
 
+		setFilter();
 	}
 }
