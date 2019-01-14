eb2de2bd7b9fdd4fb20e960e2bba33b43e0d2ca1
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2019-01-14 11:50:03 +0100

    oups

--- src/abstractDAO/AbstractUserDAO.java
+++ src/abstractDAO/AbstractUserDAO.java
@@ -6,6 +6,8 @@ import exception.DAOException;
 public abstract class AbstractUserDAO {
 
 	public abstract User login(String username, String password) throws DAOException;
+	public abstract void register(String firstname, String lastname, String username, String password) throws DAOException;
+	public abstract boolean findUsername(String username) throws DAOException;
 	public abstract void addFavoriteRecipe(int idUser, int idRecipe)throws DAOException;
 	public abstract void removeFavoriteRecipe(int idUser, int idRecipe)throws DAOException;
 
