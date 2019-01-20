f235fedc36bcdfa61ac21a64cfc0d2478db7dff4
Gardeisen <marine.gardeisen@free.fr>
2019-01-20 10:39:41 +0100

    java doc

--- src/abstractDAO/AbstractCommentDAO.java
+++ src/abstractDAO/AbstractCommentDAO.java
@@ -9,8 +9,24 @@ import exception.DAOException;
 
 public abstract class AbstractCommentDAO {
 
+	
+	/**
+	 * add a commentary to a recipe 
+	 * @param text
+	 * @param cal
+	 * @param idRecipe
+	 * @param idUser
+	 * @throws DAOException
+	 */
 	public abstract void createComment(String text, Calendar cal,int idRecipe,  int idUser) throws DAOException;
 
+	
+	/**
+	 * show all the comment of a recipe
+	 * @param idRecipe
+	 * @return a list of commentary that correspond to the recipe in param
+	 * @throws DAOException
+	 */
 	public abstract ArrayList<Commentary> showcomment(int idRecipe) throws DAOException;
 
 
